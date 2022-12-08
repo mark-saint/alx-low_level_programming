e0cdb616d1f5ea14bdd9a6971a6a536f037b0533
mark-saint <markmphillip@gmail.com>
2022-12-08 13:39:03 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - 'sth'
+*
+*Description: 'sth'
+*
+*Return: (1)
+*/
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
