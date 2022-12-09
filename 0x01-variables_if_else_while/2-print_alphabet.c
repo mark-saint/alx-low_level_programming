98ed84c9b675e6ab226bfd6bbfcca6e701b5377d
mark-saint <markmphillip@gmail.com>
2022-12-09 14:31:30 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -sth
+ * Description: sth
+ * Return: sth
+ */
+
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+
+return (0);
+}
