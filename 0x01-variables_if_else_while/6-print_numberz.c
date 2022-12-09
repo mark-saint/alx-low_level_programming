166984b99afd173839338444fd204c6977eb8a7c
mark-saint <markmphillip@gmail.com>
2022-12-09 13:45:40 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main -sth
+*Description: sth
+*Return: 0
+*/
+
+int main(void)
+{
+
+int x;
+
+for (x = 0; x <= 9; x++)
+{
+putchar('0' + x);
+}
+putchar('\n');
+return (0);
+}
