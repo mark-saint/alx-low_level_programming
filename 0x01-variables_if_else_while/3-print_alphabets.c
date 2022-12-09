a4457343eaa41b20a27fff2a802daefc02d6da4c
mark-saint <markmphillip@gmail.com>
2022-12-09 13:12:14 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main -sth
+*Description: sth
+*
+*Return: sth
+*/
+int a;
+int b;
+int main(void)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+
+putchar(a);
+}
+
+for (b = 'A'; b <= 'Z'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
