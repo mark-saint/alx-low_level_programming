6a7ea106563a26aa331118f9e138b379399fc258
mark-saint <markmphillip@gmail.com>
2022-12-13 18:54:13 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+unsigned long a = 0, b = 1, sum_;
+float total;
+
+while (sum_ <= 4000000)
+{
+sum_ = a + b;
+//if (sum_ >4000000)
+//break;
+
+if ((sum_ % 2) == 0)
+{
+total += sum_;
+}
+
+a = b;
+b = sum_;
+
+}
+
+printf("%.0f\n", total);
+
+return (0);
+
+}
