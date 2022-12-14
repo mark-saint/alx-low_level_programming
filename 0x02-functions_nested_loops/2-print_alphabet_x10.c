9f500c1821714b0dd5bca6e6d1f6533f70643627
mark-saint <markmphillip@gmail.com>
2022-12-13 16:30:21 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 :sth
+*Descrition:sth
+*/
+
+void print_alphabet_x10(void)
+{
+int count;
+char x;
+for (count=0; count < 10; count++)
+{
+for (x = 'a'; x<='z'; x++)
+_putchar(x);
+}
+_putchar('\n');
+}
