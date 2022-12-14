c054d5f53ab4d1f02f538ffb73dee4094dab81f8
mark-saint <markmphillip@gmail.com>
2022-12-13 16:53:25 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isalpha -sth
+*@c:sth
+*/
+
+int _isalphs(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
