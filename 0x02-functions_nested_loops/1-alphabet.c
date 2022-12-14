781f9cfbb3cbb1361e2562a711df3f3a9dc57aac
mark-saint <markmphillip@gmail.com>
2022-12-13 16:14:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main -sth
+* Description: sth
+* Return: sth
+*/
+int main(void)
+{ int x;
+for (x = 'a'; x <= 'z'; x++) {
+_putchar(x);
+}
+_putchar('\n'); return (0);
+}
