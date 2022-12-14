ffbe79eda167c1ba106d9539f1c0b793148c7002
mark-saint <markmphillip@gmail.com>
2022-12-13 18:06:43 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 if (n >= 98)
 {
-for (; n != 98; n--)
+for (; n > 98; n--)
 {
 printf("%d, ", n--);
 }
@@ -18,7 +18,7 @@ printf("%d\n", n);
 
 else
 {
-for (; n != 98; n++)
+for (; n < 98; n++)
 {
 printf("%d, ", n++);
 }
