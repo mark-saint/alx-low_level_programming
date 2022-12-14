d597d2428bd2f7b4c3b573aecad61a0ae2877035
mark-saint <markmphillip@gmail.com>
2022-12-13 17:16:37 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_last_digit- sth
+*Description: sth
+*/
+
+int print_last_digit(int x)
+{
+int y = x % 10;
+
+if (x < 0)
+{
+y *= -1;
+}
+
+_putchar('0' + y);
+return y;
+}
