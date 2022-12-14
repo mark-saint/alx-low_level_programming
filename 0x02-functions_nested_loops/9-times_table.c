0ca54be8528f981999886ed83f2a9c0c16d1d0e2
mark-saint <markmphillip@gmail.com>
2022-12-13 17:35:42 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*times_table- sth
+*Descrction: sth
+*/
+
+void times_table(void)
+{
+int count1 ,count2, product;
+
+for (count1 = 0; count1 <= 10; count1++)
+{
+_putchar('0')
+
+for (count2 = 1; count2 <= 10; count2++)
+{
+_putchar(',');
+_putchar(' ');
+product = count1 * count2
+
+if (product < 10)
+
+_putchar(' ');
+else
+{
+_putchar('0' + product / 10))
+}
+
+_putchar('0' + product %10)
+}
+
+_putchar('\n')
