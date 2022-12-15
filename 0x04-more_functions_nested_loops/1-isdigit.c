8be239b610768977c02d2badf5c14707b9f51b9e
marconi <007markphillip@gmail.com>
2022-12-15 23:51:55 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - sth
+ *Description: sth
+ * @c: sth
+ * Return: number
+ */
+int _isdigit(int c)
+{
+if (isdigit(c))
+return (1);
+else
+return (0);
+
+}
