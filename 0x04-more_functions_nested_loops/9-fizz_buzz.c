83fe466c64f2b2392e5c3ce585a3d9443af87b15
marconi <007markphillip@gmail.com>
2022-12-16 02:23:07 +0300

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - sth
+ * Description: sth
+ * Return: sth
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3) == 0)
+printf("Fizz ");
+else if ((i % 5) == 0)
+printf("Buzz ");
+
+else if ((i % 3) == 0 && (i % 5) == 0)
+printf("FizzBuzz ");
+
+else
+printf("%d ", i);
+
+}
+putchar('\n');
+return (0);
+}
