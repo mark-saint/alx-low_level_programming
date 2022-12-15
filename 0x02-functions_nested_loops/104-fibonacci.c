c4d0b57611e422c70ff79fc5296a5dcef3770e2b
marconi <007markphillip@gmail.com>
2022-12-15 04:30:53 +0300

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,17 +11,14 @@ int main(void)
 unsigned long a = 0, b = 1, sum_;
 unsigned long high_a, low_a, high_b, low_b, high_sum, low_sum;
 int count;
-
 for (count = 0; count < 92; count++)
 {
-    sum_ = a + b;
-    printf("%lu, ", sum_);
+sum_ = a + b;
+printf("%lu, ", sum_);
 
-    a = b;
-    b = sum_;
+a = b;
+b = sum_;
 }
-
-
 high_a = a % 10000000000;
 low_a = a /  10000000000;
 high_b = b % 10000000000;
@@ -31,7 +28,6 @@ for (count = 92; count < 98; count++)
 {
 high_sum = high_a + high_b;
 low_sum = low_a + low_b;
-
 if (high_sum > 10000000000)
 {
 low_sum += 1;
