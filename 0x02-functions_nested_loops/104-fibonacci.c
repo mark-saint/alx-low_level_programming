e1ac5b1cc8783a423517e23906109937aafcaebd
marconi <007markphillip@gmail.com>
2022-12-15 03:31:22 +0300

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,44 +11,33 @@ int main(void)
 unsigned long a = 0, b = 1, sum_;
 unsigned long high_a, low_a, high_b, low_b, high_sum, low_sum;
 int count;
+high_a = a % 10000000000;
+low_a = a /  10000000000;
+high_b = b % 10000000000;
+low_b = b /  10000000000;
 
-for (count = 0; count < 92; count++ )
+for (count = 0; count < 98; count++)
 {
-sum_ = a + b;
-printf("%lu", sum_);
-putchar(',');
-putchar(' ');
-
-a = b;
-b = sum_;
-
-}
-high_a = a % 100000000000;
-low_a = a /  100000000000;
-high_b = b % 100000000000;
-low_b = b /  100000000000;
-
-for (count = 92; count < 98; count++)
-{
-
 high_sum = high_a + high_b;
-low_sum = low_a + low_b;
 
+low_sum = low_a + low_b;
+if (high_sum>=10000000000)
+{
+low_sum+=1;
+high_sum%=10000000000;
+}
+if (low_sum != 0)
 printf("%lu", low_sum);
 printf("%lu", high_sum);
 
 if (count != 97 )
-{
-putchar(',');
-putchar(' ');
-}
+printf(", ");
+
 high_a = high_b;
 low_a = low_b;
 high_b = high_sum;
 low_b = low_sum;
-
 }
-
 putchar('\n');
 return (0);
 }
