da6b75778a8d345e5997c8855599ef200532c0e5
marconi <007markphillip@gmail.com>
2022-12-16 05:40:30 +0300

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_number - sth
  * Description: sth
@@ -12,8 +13,9 @@ if (n < 0)
 _putchar('-');
 n = -n;
 }
+
 if (n / 10)
 print_number(n / 10);
 
-_putchar(n % 10 + '0');
+_putchar((n % 10) + '0');
 }
