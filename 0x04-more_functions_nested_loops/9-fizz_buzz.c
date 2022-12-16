ce90ba6e46a25ac3d48e41a85df66fecffe51679
marconi <007markphillip@gmail.com>
2022-12-16 04:48:40 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,19 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3) == 0)
-printf("Fizz ");
+if ((i % 3) == 0 && (i % 5) == 0)
+printf("FizzBuzz ");
 
 else if ((i % 5) == 0)
 printf("Buzz ");
 
-else if ((i % 3) == 0 && (i % 5) == 0)
-printf("FizzBuzz ");
+else if ((i % 3) == 0)
+printf("Fizz ");
 
 else
 printf("%d ", i);
 }
+
 putchar('\n');
 return (0);
 }
