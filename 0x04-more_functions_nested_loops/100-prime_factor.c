449db25b19dbcbc9e2762b31c7b300a65c1aacd3
marconi <007markphillip@gmail.com>
2022-12-16 03:28:11 +0300

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - sth
+* Descrition: sth
+* Return: sth
+*/
+
+int main(void)
+{
+unsigned long i = 2;
+unsigned long num = 612852475143;
+unsigned long keep;
+
+while (num > 1)
+{
+if (num % i == 0)
+{
+keep = i;
+num /= i;
+}
+else
+i++;
+}
+printf("%lu ",keep);
+putchar('\n');
+return (0);
+}
