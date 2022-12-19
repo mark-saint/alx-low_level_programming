e6231e27b1fb844e2be0503cb0a65fe5b8aac150
marconi <007markphillip@gmail.com>
2022-12-20 02:21:48 +0300

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _puts - funtion
- * @str: param
- * Description: sth
- */
+* _puts - funtion
+* @str: param
+* Description: sth
+*/
 void _puts(char *str)
 {
-    int a;
-    a = 0;
+int a;
+a = 0;
 
-    while(str[a] != '\0')
-    {
-        _putchar(str[a]);
-        a++;
-    }
-    _putchar('\n');
+while(str[a] != '\0')
+{
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
 
 }
