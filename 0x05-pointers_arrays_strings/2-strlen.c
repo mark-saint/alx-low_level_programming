84e1fdaf534804a61dc19a8fd547af0190fe1903
marconi <007markphillip@gmail.com>
2022-12-20 01:54:52 +0300

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen - function
- * @s: param pointer
- * return last value
- */
+* _strlen - function
+* Descrition: returns len of str
+* @s: param pointer
+* Return: last value of the loop
+*/
 
 int _strlen(char *s)
 {
-    int a;
-    a=0;
-    while (*s++)
-    {
-    a++;
-    }
+int a;
+a=0;
+while (*s++)
+{
+a++;
+}
 
-    return (a);
+return (a);
 }
