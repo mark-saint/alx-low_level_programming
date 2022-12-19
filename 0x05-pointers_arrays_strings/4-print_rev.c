bda51a899f2821b3ddcab4d0209e8957452b7621
marconi <007markphillip@gmail.com>
2022-12-20 02:43:07 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * _strlen - function
+ * Description: returns len of str
+ * @s: param pointer
+ * Return: last value of the loop
+ */
+
+int _strlen(char *s)
+{
+int a;
+a = 0;
+while (*s++)
+a++;
+
+return (a);
+}
+
+/**
+ * print_rev - function2
+ * Description: sth
+ * @s: param
+ */
+
+void print_rev(char *s)
+{
+int len;
+len = _strlen(s);
+
+while (len >= 0)
+{
+_putchar(s[len]);
+len--;
+}
+_putchar('\n');
+}
