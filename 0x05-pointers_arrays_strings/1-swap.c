5e09c5a54e9cc0ec133cec494a68535cb56ad825
marconi <007markphillip@gmail.com>
2022-12-20 01:18:12 +0300

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - function
- * Description: swaps
- * @a: int
- * @b: int
- */
+* swap_int - function
+* Description: swaps
+* @a: int
+* @b: int
+*/
 void swap_int(int *a, int *b)
 {
-    int c;
-    c = *a;
+int c;
+c = *a;
 
-    *a = *b;
-    *b = c;
+*a = *b;
+*b = c;
 }
