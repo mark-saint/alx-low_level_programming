7175f1c7b49355825b696461afc9e3ea6a88067c
marconi <007markphillip@gmail.com>
2022-12-20 05:04:45 +0300

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,13 +12,13 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-    printf("%d", a[i]);
-    if (i != n - 1)
-    {
-        putchar(',');
-        putchar(' ');
-    }
+printf("%d", a[i]);
+if (i != n - 1)
+{
+putchar(',');
+putchar(' ');
+}
 
 }
-    putchar('\n');
+putchar('\n');
 }
