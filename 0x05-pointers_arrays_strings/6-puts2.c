c3b2d4d903e5afadf788006bf1ad48b6c3c7a344
marconi <007markphillip@gmail.com>
2022-12-20 04:16:14 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - sth
+ * @str: param
+ * Description: sth
+ */
+void puts2(char *str)
+{
+int a;
+a = 0;
+while (str[a] != '\0')
+{
+if (a % 2 == 0)
+_putchar(str[a]);
+a++;
+}
+}
