d6cc71aad8146a5b74ac560e0658599c94773e13
marconi <007markphillip@gmail.com>
2022-12-20 04:33:46 +0300

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+
+/**
+* _strlen - function
+* Descrition: returns len of str
+* @s: param pointer
+* Return: last value of the loop
+*/
+
+int _strlen(char *s)
+{
+int a;
+a = 0;
+while (*s++)
+{
+a++;
+}
+
+return (a);
+}
+
+/**
+ * puts_half - function
+ * Description: sth
+ * @str: sth
+ */
+void puts_half(char *str)
+{
+int len, a;
+len = _strlen(str);
+
+if ((len % 2) == 0)
+a = len / 2;
+else
+a = (len - 1) / 2;
+
+while (str[a] != '\0')
+{
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
+}
