851d80673df6c1fe2d8b61aeb10b925a43f7676f
marconi <007markphillip@gmail.com>
2022-12-20 04:57:58 +0300

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - function
+* @a: param
+* @n: param
+* Description: sth
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d, ", a[i]);
+}
+}
