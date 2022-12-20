2a1b6971d9595467f29870bc5a07db05266e8c9e
marconi <007markphillip@gmail.com>
2022-12-20 05:47:55 +0300

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - function
+ * Description: sth
+ * @s: param
+ * Return: converted number
+ */
+
+int _atoi(char *s)
+{
+unsigned int b = 0;
+int a = 1, count = 0, to_break = 0;
+
+
+while (1)
+{
+if (s[count] == '-')
+a *= -1;
+
+while (s[count] >= 48 && s[count] <= 57)
+{
+b = b * 10 + (s[count] - 48);
+count++;
+to_break = 1;
+}
+
+if (to_break == 1)
+break;
+
+count++;
+}
+b *= a;
+
+return (b);
+}
