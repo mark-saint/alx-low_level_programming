e00e8d3897e9aa7eb1dfdb211ee72ea1914166d4
marconi <007markphillip@gmail.com>
2022-12-20 05:12:19 +0300

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcpy - function
+* Description: sth
+* @dest: param
+* @src: param
+* Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[a] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
