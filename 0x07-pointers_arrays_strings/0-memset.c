b88bd7067d51947b014b456a6e59a6137aa56bf7
marconi <007markphillip@gmail.com>
2023-01-03 00:32:00 +0300

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memset - memset function
+* @s: param
+* @b: param
+* @n: param
+* Description: memset function
+* Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+a = 0;
+while (a < n)
+{
+s[a] = b;
+a++;
+}
+return (s);
+}
