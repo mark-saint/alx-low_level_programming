fa1b105fab40b396088226c092f9044c74a55cd3
marconi <007markphillip@gmail.com>
2023-01-03 01:44:00 +0300

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * _strchr - function
- * @s: param
- * @c: param
- * Description: sth
- * Return: sth
- */
+* _strchr - function
+* @s: param
+* @c: param
+* Description: sth
+* Return: sth
+*/
 char *_strchr(char *s, char c)
 {
 while (s++)
 {
 if (*s == c)
 return (s);
-
+}
 if (c == '\0')
 return (s);
-}
+
 return (NULL);
 }
