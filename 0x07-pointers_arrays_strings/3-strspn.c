8cc3184e5d96c6144ccbb6d86d37fbec93689857
marconi <007markphillip@gmail.com>
2023-01-03 04:43:33 +0300

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - function
+ * @s: param
+ * @accept: param
+ * Return: sth
+ * Description: sth
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int  a = 0, b = 0;
+int counter = 0;
+
+while (s[a] != '\0')
+{
+if (s[a] != 32)
+{
+while (accept[b] != '\0')
+{
+printf("%c", accept[b]);
+if (s[a] == accept[b])
+counter++;
+b++;
+}
+}
+else
+return (counter);
+a++;
+b = 0;
+}
+return (counter);
+}
