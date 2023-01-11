7c2a7068d93a59f2e989294eea09f7f65aa30f92
marconi <007markphillip@gmail.com>
2023-01-11 20:18:18 +0300

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - argc argv
+* @argc: num arguments
+* @argv: arguments
+* Description: read
+* Return: (0);
+*/
+int main(int argc, char **argv)
+{
+int sum = 0;
+int i = 1;
+
+if (argc == 1)
+{
+printf("0\n");
+}
+
+else
+{
+while (i < argc)
+{
+
+if (isdigit(argv[i][0]) != 0)
+{
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+i++;
+}
+printf("%d\n", sum);
+}
+
+
+return (0);
+}
