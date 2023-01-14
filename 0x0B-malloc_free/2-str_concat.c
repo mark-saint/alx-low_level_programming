52f8c1c6d8b2cc46fc1eebab1d0435591f878c58
marconi <007markphillip@gmail.com>
2023-01-14 23:01:35 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,27 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+* str_concat - fn
+* @s1: param
+* @s2: param
+* Return: sth
+* Description: sth
+*/
 char *str_concat(char *s1, char *s2)
 {
-    char *new;
-    int a = 0, b = 0, i = 0;
-     while (s1[a])
-        a++;
-    a++;
+char *new;
+int a = 0, b = 0, i = 0;
 
-    while (s2[b])
-        b++;
-    b++;
+if (s2 == NULL)
+s2 = "";
+if (s1 == NULL)
+s1 = "";
 
-    new = malloc(sizeof(*new) * (a + b));
+while (s1[a])
+{
+a++;
+}
+a++;
 
-    *new = *s1;
-    printf("%s",s1);
-    while (i < b)
-    {
-        new[i] = s2[i];
-        i++;
-    }
+while (s2[b])
+{
+b++;
+}
+b++;
 
-    return (new);
+new = malloc(sizeof(*new) * (a + b));
+
+*new = *s1;
+printf("%s",s1);
+while (i < b)
+{
+new[i] = s2[i];
+i++;
+}
+*new = '\0';
+return (new);
 }
