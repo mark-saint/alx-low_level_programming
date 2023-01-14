22e37f61fb2b3fbfeed0ef2a153ebbb18c843015
marconi <007markphillip@gmail.com>
2023-01-15 02:49:59 +0300

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _realloc - fn
+* @ptr: param
+* @old_size: param
+* @new_size: param
+* Return: sth
+* Description: sth
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned int i;
+char *new_ptr;
+char *p = ptr;
+
+if (p == NULL)
+{
+new_ptr = malloc(new_size);
+return (new_ptr);
+}
+
+if (new_size == old_size)
+{
+return (ptr);
+}
+
+if (new_size == 0 && p != NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < new_size; i++)
+{
+new_ptr[i] = p[i];
+}
+free(p);
+
+return (new_ptr);
+
+}
