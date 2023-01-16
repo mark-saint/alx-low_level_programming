205666345e6715e0429cb78d34fa990ee0bfeea5
marconi <007markphillip@gmail.com>
2023-01-17 01:00:25 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - struct
+* @name: name
+* @age: age
+* @owner: owner
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
