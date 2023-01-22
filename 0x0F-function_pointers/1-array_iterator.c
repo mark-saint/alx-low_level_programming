b8b0374f739ea8eb634a667273925ab664e7d665
marconi <007markphillip@gmail.com>
2023-01-23 02:30:15 +0300

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,12 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-unsigned int i;
+size_t i;
 
-for (i = 0; i < size; i++)
+if (array != NULL && action != NULL)
 {
+for (i = 0; i < size; i++) {
 action(*(array + i));
+}
 }
 }
