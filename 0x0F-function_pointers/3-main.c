6440b8cd620b3ec07afd2034a03428ca65ee88ad
marconi <007markphillip@gmail.com>
2023-01-23 03:47:59 +0300

    trouble

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,15 +16,19 @@ if (argc > 4)
 printf("Error\n");
 exit(98);
 }
+
 x = atoi(argv[1]);
 y = atoi(argv[3]);
 
 f = get_op_func(argv[2]);
 
+if (f == NULL)
+{
+    printf("Error\n");
+    exit(99);
+}
 z = f(x, y);
 
 printf("%d\n", z);
-
-
 return (0);
 }
