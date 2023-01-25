f65bf125b450296197f90278e5dd01cf5174e831
marconi <007markphillip@gmail.com>
2023-01-25 18:31:09 +0300

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - fn
+* @n: param
+* Return: sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+int s = 0;
+unsigned int i;
+
+va_list ptr;
+
+va_start(ptr, n);
+
+for (i = 0; i < n; i++)
+{
+s += va_arg(ptr, int);
+}
+
+va_end(ptr);
+
+return (s);
+}
