a6ef4b8c5b2fbce9e37a6e4b9fc2044f55b8b247
marconi <007markphillip@gmail.com>
2023-01-25 18:33:36 +0300

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
-if (n == 0)
-return (0);
-
 int s = 0;
 unsigned int i;
 
 va_list ptr;
 
+if (n == 0)
+return (0);
 va_start(ptr, n);
 
 for (i = 0; i < n; i++)
