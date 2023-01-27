0150864d04edaba45e2e4702973bd50a494d7e75
marconi <007markphillip@gmail.com>
2023-01-27 04:09:41 +0300

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+* add_node: fn
+* @head: previous element
+* @str: str
+* Description: sth
+* Return: next element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new;
+
+if (str == NULL)
+{
+return (NULL);
+}
+unsigned int i = 0;
+while (str[i])
+i++;
+
+new = malloc(sizeof(list_t));
+new->str = strdup(str);
+
+new->len = i;
+
+new->next = *head;
+*head = new;
+return (new);
+
+}
