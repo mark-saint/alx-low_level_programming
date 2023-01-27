b935771d02f7d5d8a0bfb4517085362f0da304b1
marconi <007markphillip@gmail.com>
2023-01-27 05:04:56 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+* before_main - print statement before main
+*/
+void before_main(void)
+{
+printf("You're beat! and yet, ");
+printf("you must allow,\nI bore my house upon my back!\n");
+}
