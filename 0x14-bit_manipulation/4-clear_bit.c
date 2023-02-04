0723e18a3817cec7b5e67471f8c172407c4643d9
marconi <007markphillip@gmail.com>
2023-02-04 04:45:20 +0300

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int x;
 
+if (index > sizeof(unsigned long int) * 8 - 1)
+return (-1);
+
 x = ~(1 << index);
 *n = *n & x;
 
