dcf9484d1f45288f57052f7c49d016945c0b9917
marconi <007markphillip@gmail.com>
2023-02-04 03:42:51 +0300

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* get_bit - fn
+* @n: num
+* @index: index
+* Return: bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int x, y;
+
+
+
+y = 1 << index;
+
+x = n & y;
+if (x == y)
+{
+return (1);
+}
+return (0);
+}
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,5 +3,9 @@
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+
 
 #endif
